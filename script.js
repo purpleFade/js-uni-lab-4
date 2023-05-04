7c2ed904b0020cfd5b032689efa9c9d06e6ecc1e
Anton Tarovyk <57455737+purpleFade@users.noreply.github.com>
2023-05-04 18:22:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 document.getElementById('button').addEventListener('click', tangent)
 
 async function tangent() {
+  try {
     const x = document.getElementById('inputX').value
     const func = document.getElementById('inputFunc').value
-    const respons = await fetch(`https://newton.vercel.app/api/v2/tangent/${x}|${func}`).then(data => data.json())
-    document.getElementById("demo").innerHTML = respons.result;
+    const response = await fetch(`https://newton.vercel.app/api/v2/tangent/${x}|${func}`)
+    if (response.ok) {
+      const data = await response.json()
+      document.getElementById("demo").innerHTML = data.result
+    } else {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    tangent()
   }
-  
+}
